467cbbd90b9afabc68fe22e355906fb5ffcff87b
clw <liangweicai@upchina.com>
2024-01-11 14:18:53 +0800

    增加克隆方法

--- 面试题实践/index.js
+++ 面试题实践/index.js
@@ -1,18 +1,18 @@
 // 能够判断所有数据类型的方法
 function getType(obj) {
-    let type = typeof obj;
-    if (type !== "object") {
-        return type;
-    }
-    return Object.prototype.toString.call(obj).slice(8, -1);
+  let type = typeof obj;
+  if (type !== "object") {
+    return type;
+  }
+  return Object.prototype.toString.call(obj).slice(8, -1);
 }
 
 // reduce的作用，Obj转化成一个二维数组
 function objToArr(obj) {
-    return Object.keys(obj).reduce((arr, key) => {
-        arr.push([key, obj[key]]);
-        return arr;
-    }, []);
+  return Object.keys(obj).reduce((arr, key) => {
+    arr.push([key, obj[key]]);
+    return arr;
+  }, []);
 }
 
 // vite与webpack的区别
@@ -22,18 +22,79 @@ function objToArr(obj) {
 // 数组扁平化
 // 1. reduce
 function flatten(arr) {
-    return arr.reduce((pre, cur) => {
-      return Array.isArray(cur) ? [...pre, ...flatten(cur)] : [...pre, cur];
-    })
+  return arr.reduce((pre, cur) => {
+    return Array.isArray(cur) ? [...pre, ...flatten(cur)] : [...pre, cur];
+  })
 }
 // 2. toString
 function flatten(arr) {
-    return arr.toString().split(",").map((item) => {
-        return +item;
-    });
+  return arr.toString().split(",").map((item) => {
+    return +item;
+  });
 }
 // 3. flat
 function flatten(arr) {
-    return arr.flat(Infinity);
+  return arr.flat(Infinity);
 }
 
+// 数组去重
+// 1. set
+function unique(arr) {
+  return [...new Set(arr)];
+}
+// 2.filter
+function unique(arr) {
+  return arr.filter((item, index) => {
+    return arr.indexOf(item) === index
+  })
+}
+// 3. reduce
+function unique(arr) {
+  return arr.reduce((pre, cur) => {
+    return pre.includes(cur) ? pre : [...pre, cur];
+  }, [])
+}
+// 4. map
+function unique(arr) {
+  let map = new Map();
+  return arr.filter((item) => {
+    return !map.has(item) && map.set(item, 1);
+  })
+}
+
+// 递归的方法深拷贝,加上注释
+function deepClone(obj) {
+  if (obj instanceof Date) return new Date(obj);
+  if (obj instanceof RegExp) return new RegExp(obj);
+  if (obj === null || typeof obj !== 'object') return obj;
+
+  const cloneObj = Array.isArray(obj) ? [] : {};
+  for (let key in obj) {
+    if (obj.hasOwnProperty(key)) {  // 是否是自身属性
+      cloneObj[key] = deepClone(obj[key]);
+    }
+  }
+  return cloneObj;
+}
+
+// 柯里化函数
+function curry(func) {
+  return function curried(...args) {
+    if (args.length >= func.length) {
+      return func(...args);
+    } else {
+      return function (...newArgs) {
+        return curried(...args, ...newArgs);
+      };
+    }
+  };
+}
+
+// 使用方法
+function sum(a, b, c) {
+  return a + b + c;
+}
+
+let curriedSum = curry(sum);
+
+console.log(curriedSum(1)); // 输出 6
